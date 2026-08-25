856a11ea1332692bacd8d84965d734a034b94542
Suraj Mishra <75621217+imsuraj024@users.noreply.github.com>
2026-08-25 09:01:57 +0530

    Add networking example app

---
+++ examples/networking/repository-app/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: UserPage()));
+
+class User {
+  const User(this.name);
+
+  final String name;
+}
+
+abstract interface class UserDataSource {
+  User fetchUser();
+}
+
+class FakeUserDataSource implements UserDataSource {
+  @override
+  User fetchUser() => const User('Ada Lovelace');
+}
+
+class UserRepository {
+  const UserRepository(this.dataSource);
+
+  final UserDataSource dataSource;
+
+  User getUser() => dataSource.fetchUser();
+}
+
+class UserPage extends StatelessWidget {
+  const UserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const repository = UserRepository(FakeUserDataSource());
+    final user = repository.getUser();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Networking Boundary')),
+      body: Center(child: Text(user.name)),
+    );
+  }
+}
